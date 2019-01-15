88738793354f6e79f1dfea9b30a83e836e355a5a
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2019-01-15 14:35:08 +0100

    Ajout de la classe de test

---
+++ src/main/java/TestOntology.java
@@ -0,0 +1,17 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+/**
+ * Tests de l'ontologie domotic
+ */
+public class TestOntology {
+    @BeforeClass
+    public void oneTimeSetUp() throws Exception {
+        // TODO : charger l'ontologie et les règles
+    }
+
+    @AfterClass
+    public void oneTimeTearDown() throws Exception {
+        // TODO : clean all
+    }
+}
