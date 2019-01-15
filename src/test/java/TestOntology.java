1e9a38619729fde11452b5e0f36b719c650cdca7
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2019-01-15 14:52:25 +0100

    Ajout des tools et premiers tests

---
+++ src/test/java/TestOntology.java
@@ -0,0 +1,32 @@
+import com.hp.hpl.jena.rdf.model.Model;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import tools.JenaEngine;
+
+/**
+ * Tests de l'ontologie domotic
+ */
+public class TestOntology {
+
+    /**
+     * Ontology
+     */
+    private static Model model;
+
+    @BeforeClass
+    public static void oneTimeSetUp() throws Exception {
+        // TODO : charger l'ontologie et les règles
+        model = JenaEngine.readModel("src/test/resources/domotic-projet.owl");
+    }
+
+    @AfterClass
+    public static void oneTimeTearDown() throws Exception {
+        // TODO : clean all
+    }
+
+    @Test
+    public void a() {
+        System.out.println("AH");
+    }
+}
